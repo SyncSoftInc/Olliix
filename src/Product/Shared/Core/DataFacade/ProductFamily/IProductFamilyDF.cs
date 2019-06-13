616f486962e3fab43a98205ded9c6ab326b33120
Lukiya Chen <lukiya@live.com>
2019-06-13 13:56:09 -0700

    update

---
+++ src/Product/Shared/Core/DataFacade/ProductFamily/IProductFamilyDF.cs
@@ -0,0 +1,10 @@
+﻿using SyncSoft.Olliix.Product.DTO.ProductFamily;
+using System.Threading.Tasks;
+
+namespace SyncSoft.Olliix.Product.DataFacade.ProductFamily
+{
+    public interface IProductFamilyDF
+    {
+        Task<ProductFamilyDTO> GetProductFamilyAsync(string familyId);
+    }
+}
